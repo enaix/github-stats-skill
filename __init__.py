2eab27e0d379af836085b965b0864cdf8cb26ad7
root <root@local.host>
2021-12-22 18:03:22 +0300

    Add time of the day func

--- __init__.py
+++ __init__.py
@@ -1,5 +1,6 @@
 from mycroft import MycroftSkill, intent_handler
 import re
+from datetime import datetime
 
 
 class GithubStats(MycroftSkill):
@@ -9,11 +10,22 @@ class GithubStats(MycroftSkill):
     def tell_stats(self, message):
         pass
 
+    def get_time(self):
+        t = datetime.now().hour
+        if t > 5 and t <= 12:
+            return "morning"
+        elif t > 12 and t <= 17:
+            return "day"
+        elif t > 17 and t <= 23:
+            return "evening"
+        else:
+            return "night"
+
     def greet(self, message):
         if re.match(self.translate_regex('is.awake'), message):
             self.speak_dialog('awake.dialog')
         else:
-            self.speak_dialog('greet.dialog')
+            self.speak_dialog('greet.dialog', data={'time': self.get_time()})
 
     @intent_handler('stats.github.intent')
     def handle_stats_github(self, message):
